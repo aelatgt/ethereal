2df038d5bd8a6c16af0b1dd350225d9a9a76213e
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-01-21 14:20:01 -0500

    Optimize getting canvas image data

--- packages/web-layer/core/WebLayerCache.ts
+++ packages/web-layer/core/WebLayerCache.ts
@@ -1,10 +1,8 @@
 import { Bounds, Edges } from "./dom-utils"
 import Dexie, { Table, liveQuery, Subscription } from 'dexie'
-import { load } from '@loaders.gl/core';
-import { ImageLoader, ImageDataType } from '@loaders.gl/images';
 
 // @ts-ignore
-import { KTX2Encoder } from './textures/KTX2Encoder.bundle.js';
+import KTX2Encoder from './textures/KTX2Encoder.bundle.js';
 import type { KTX2Encoder as KTX2EncoderType } from './textures/KTX2Encoder'
 
 export type StateHash = string
@@ -20,6 +18,7 @@ export interface LayerStateData {
 
 export interface TextureData {
     hash: TextureHash
+    renderAttempts: number
     lastUsedTime: number
     texture?: ArrayBuffer
 }
@@ -45,6 +44,7 @@ export class WebLayerCache {
     
     private _textureStore:TextureStore
     private _textureUrls = new Map<TextureHash, string>()
+    private _textureData = new Map<TextureHash, TextureData>()
     private _textureSubscriptions = new Map<TextureHash, Subscription>()
     private _layerStateData = new Map<StateHash, LayerStateData>()
     private _encoder = new KTX2Encoder() as KTX2EncoderType
@@ -62,19 +62,19 @@ export class WebLayerCache {
     }
 
     async updateTexture(textureHash:TextureHash, canvas:HTMLCanvasElement) {
-        return new Promise((resolve) => {
-            canvas.toBlob(async (blob) => {
-                if (!blob) return resolve(null)
-                try {
-                    const imageData = await load(blob, ImageLoader, {image: {type: 'data'}}) as ImageDataType
-                    const ktx2Texture = await this._encoder.encode(imageData)
-                    const textureData = await this._textureStore.textures.get(textureHash) || {hash: textureHash, lastUsedTime:Date.now(), texture:undefined}
-                    textureData.texture = ktx2Texture
-                    this._textureStore.textures.put(textureData)                
-                } finally {
-                    resolve(null)
-                }
-            })
+        return new Promise(async (resolve) => {
+            try {
+                const context = canvas.getContext('2d')!
+                const imageData = context.getImageData(0,0,canvas.width, canvas.height)
+                const ktx2Texture = await this._encoder.encode(imageData as any)
+                const textureData = await this._textureStore.textures.get(textureHash) || 
+                    {hash: textureHash, renderAttempts: 0, lastUsedTime:Date.now(), texture:undefined}
+                textureData.texture = ktx2Texture
+                textureData.renderAttempts++
+                this._textureStore.textures.put(textureData)                
+            } finally {
+                resolve(null)
+            }
         })
     }
 
@@ -84,10 +84,15 @@ export class WebLayerCache {
         if (!this._textureSubscriptions.has(textureHash)) {
 
             const subscription = liveQuery(() => this._textureStore.textures.get(textureHash)).subscribe((value) => {
-                if (value && value.texture) {
+                if (value) {
+                    this._textureData.set(textureHash, value)
                     const previousBlobUrl = this._textureUrls.get(textureHash)
                     if (previousBlobUrl) URL.revokeObjectURL(previousBlobUrl)
-                    this._textureUrls.set(textureHash, URL.createObjectURL(new Blob([value.texture], {type: 'image/ktx2'})))
+                    if (value.texture) {
+                        this._textureUrls.set(textureHash, URL.createObjectURL(new Blob([value.texture], {type: 'image/ktx2'})))
+                    } else {
+                        this._textureUrls.delete(textureHash)
+                    }
                 }
             })
 
@@ -95,6 +100,11 @@ export class WebLayerCache {
         }
 
         return this._textureStore.textures.get(textureHash)
+    }
+
+    getTextureData(textureHash:TextureHash) {
+        if (!this._textureSubscriptions.has(textureHash)) this.requestTextureData(textureHash)
+        return this._textureData.get(textureHash)
     }
 
     getTextureURL(textureHash:TextureHash) {
